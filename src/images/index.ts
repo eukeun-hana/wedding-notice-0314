9462da8194854adeb9c2520f29149e5ece79e7d7
eukeun-hana <connetflix@naver.com>
2026-01-26 22:08:46 +0900

    chg gallery

--- src/images/index.ts
+++ src/images/index.ts
@@ -14,9 +14,13 @@ import t9 from "./thumb/image9.webp"
 import t10 from "./thumb/image10.webp"
 import t11 from "./thumb/image11.webp"
 import t12 from "./thumb/image12.webp"
-import t13 from "./thumb/image13.webp"
+//import t13 from "./thumb/image13.webp"
 import t14 from "./thumb/image14.webp"
 import t15 from "./thumb/image15.webp"
+import t16 from "./thumb/image16.webp"
+import t17 from "./thumb/image17.webp"
+import t18 from "./thumb/image18.webp"
+import t19 from "./thumb/image19.webp"
 
 //확대용
 import f1 from "./full/image1.webp" 
@@ -31,25 +35,29 @@ import f9 from "./full/image9.webp"
 import f10 from "./full/image10.webp" 
 import f11 from "./full/image11.webp" 
 import f12 from "./full/image12.webp"
-import f13 from "./full/image13.webp" 
+//import f13 from "./full/image13.webp" 
 import f14 from "./full/image14.webp" 
 import f15 from "./full/image15.webp"
+import f16 from "./full/image16.webp"
+import f17 from "./full/image17.webp"
+import f18 from "./full/image18.webp"
+import f19 from "./full/image19.webp"
 
 export const COVER_IMAGE = coverImage
 
 export const GALLERY_THUMBS = [
-  t1, t2, t3, 
-  t4, t5, t6,
-  t7, t8, t9,
-  t10, t11, t12, 
-  t13, t14, t15 
+  t1, t2, t3, t4,
+  t14, t5, t17, t6, 
+  t11, t12,  t9, t10, 
+  t7, t8,t18, t15,
+  //t17, t18, t19
 ]
 
 export const GALLERY_FULL = [
-  f1, f2, f3,
-  f4, f5, f6,
-  f7, f8, f9,
-  f10, f11, f12,
-  f13, f14, f15
+  f1, f2, f3, f4,
+  f14,f5, f17, f6, 
+   f11, f12,f9, f10,
+  f7, f8, f18, f15,
+  //f17, f18,
 ]
 
