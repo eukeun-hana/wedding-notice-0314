484c7683747bcd98145561299a0b248b210d25af
eukeun-hana <connetflix@naver.com>
2026-01-27 22:38:28 +0900

    cdn img

--- src/images/index.ts
+++ src/images/index.ts
@@ -26,43 +26,25 @@ import t17 from "./thumb/image17.webp"
 import t18 from "./thumb/image18.webp"
 import t19 from "./thumb/image19.webp"
 
-//확대용
-import f1 from "./full/image1.webp" 
-import f2 from "./full/image2.webp" 
-import f3 from "./full/image3.webp" 
-import f4 from "./full/image4.webp"
-import f5 from "./full/image5.webp"
-import f6 from "./full/image6.webp" 
-import f7 from "./full/image7.webp"
-import f8 from "./full/image8.webp" 
-import f9 from "./full/image9.webp" 
-import f10 from "./full/image10.webp" 
-import f11 from "./full/image11.webp" 
-import f12 from "./full/image12.webp"
-//import f13 from "./full/image13.webp" 
-import f14 from "./full/image14.webp" 
-import f15 from "./full/image15.webp"
-import f16 from "./full/image16.webp"
-import f17 from "./full/image17.webp"
-import f18 from "./full/image18.webp"
-import f19 from "./full/image19.webp"
-
 export const COVER_IMAGE = coverImage
 
 export const GALLERY_THUMBS = [
   t1, t2, t3, t4,
   t14, t5, t17, t6, 
   t11, t12,  t9, t10, 
-  t7, t8, b, t15,
+  t7, t8, t15,
   //t7, t8, t18, t15,
   
 ]
 
+const IMG_BASE = "https://cdn.jsdelivr.net/gh/eukeun-hana/wedding-gallery@v1.0.0/gallery"
+
 export const GALLERY_FULL = [
-  f1, f2, f3, f4,
-  f14,f5, f17, f6, 
-  f11, f12,f9, f10,
-  f7, f8, b, f15,
-  //f7, f8, f18, f15,
+  `${IMG_BASE}/image1.webp`, `${IMG_BASE}/image2.webp`, `${IMG_BASE}/image3.webp`,
+  `${IMG_BASE}/image4.webp`, `${IMG_BASE}/image14.webp`, `${IMG_BASE}/image5.webp`,
+  `${IMG_BASE}/image17.webp`, `${IMG_BASE}/image6.webp`, `${IMG_BASE}/image11.webp`,
+  `${IMG_BASE}/image12.webp`, `${IMG_BASE}/image9.webp`, `${IMG_BASE}/image10.webp`,
+  `${IMG_BASE}/image7.webp`, `${IMG_BASE}/image8.webp`, `${IMG_BASE}/image15.webp`,
+  
 ]
 
